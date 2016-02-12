48472ca493f07bb10a7a16429fe92b3c144a51e6
Daniel <futridan@gmail.com>
2016-02-12 13:55:00 -0430

    Refactorización de Vistas y Modulos

--- resources/js/jquery/app.js
+++ resources/js/jquery/app.js
@@ -2,14 +2,6 @@
 
 //loadModule('monitor','dashboard');
 
-function loadModule(modulo,accion){
-    $.loader({
-        className:"blue-with-image-2",
-        content:''
-    });
-    $("#page-wrapper").load("views/" + modulo + "/" + accion + ".html", function(response, status, xhr){
-        //console.log(status, xhr);
-        $.loader('close');
-    });
-};
+
+
 
---
+++ resources/js/jquery/utils.js
@@ -0,0 +1,65 @@
+
+
+/**
+ * Consiguración para guardar cache de las llamadas asincronas
+ */
+$.ajaxSetup({
+    cache: true
+});
+
+/**
+ * Carga modulos html en el pag-wrapper
+ * @param modulo
+ * @param accion
+ */
+function loadModule(modulo,accion){
+    $.loader({
+        className:"blue-with-image-2",
+        content:''
+    });
+    $("#page-wrapper").load("views/" + modulo + "/" + accion + ".html", function(response, status, xhr){
+        //console.log(status, xhr);
+        $.loader('close');
+    });
+};
+
+/**
+ * Consulta lista de datos desde el backend
+ * @param url
+ * @returns {*}
+ */
+function getList(url){
+    $.loader({
+        className:"blue-with-image-2",
+        content:''
+    });
+    return $.get(
+        url,
+        {
+            apiKey:'77fa53ff60e8f41e40260b0dad826d76',
+            "_": $.now()
+        },
+        null,
+        'json'
+    );
+};
+
+/**
+ * Carga Script dinamicamente y los almacena temporalmente en cache
+ * @param url
+ * @param options
+ * @returns {*}
+ */
+jQuery.cachedScript = function( url, options ) {
+
+    // Allow user to set any option except for dataType, cache, and url
+    options = $.extend( options || {}, {
+        dataType: "script",
+        cache: true,
+        url: url
+    });
+
+    // Use $.ajax() since it is more flexible than $.getScript
+    // Return the jqXHR object so we can chain callbacks
+    return jQuery.ajax( options );
+};
