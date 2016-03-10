ae793594e87225604a02bb0ac0efc72a00bf3db7
Daniel Futrillé <futridan@gmail.com>
2016-03-10 06:46:21 -0430

    Detalles de Pantalla de Inicio

--- resources/js/jquery/app.js
+++ resources/js/jquery/app.js
@@ -1,6 +1,40 @@
+(function() {
+    'use strict';
 
+    $.loader({ className:"blue-with-image-2", content:'' });
+    var jqxhr = $.post( getRoute('homepage') , $('form[name=login]').serialize(), function(data, status, xhr) {
+        if (data.status != undefined && data.status == 'success'){
+            setMesageCode(MSG_NO_MESSAGE);
+            //window.location.href = getRoute(
+            cargarPanel();
+        }else {
+            setMesageCode(MSG_LOGIN_ERROR);
+            printMessage(getMessageCode());
+            window.location.href = 'login.html';
+        }
+    })
+        .done(function() {
 
-//loadModule('monitor','dashboard');
+        })
+        .fail(function() {
+
+        })
+        .always(function() {
+            //$.loader('close');
+        });
+    function cargarPanel(){
+        $('#wrapper').load('views/index/side-menu.html', hacerClick);
+    }
+
+    function hacerClick(){
+        $("#btn-inicio").on('click', function(){
+            console.log('se hizo click');
+            $.loader('close');
+            loadModule('monitor','monitor','Index');
+        });
+        $( "#btn-inicio" ).trigger( "click" );
+    }
+})();
 
 
 
