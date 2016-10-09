36c773445290668398a594d55b537988458e7a98
Daniel Futrillé <futridan@gmail.com>
2016-10-09 19:02:35 -0400

    Persona - Consulta
    Cambios en el listado y en el Guardar

--- resources/js/jquery/ganados/ganadosIndexController.js
+++ resources/js/jquery/ganados/ganadosIndexController.js
@@ -6,8 +6,21 @@
             "url": "dataTables.spanish.lang"
         }
     });
+
+    function getFecha(timestamp){
+        var a = new Date(timestamp * 1000);
+        var months = ['Ene','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = "0" + a.getMonth();
+        var date = "0" + a.getDate();
+
+        var time = date.substr(-2) + '/' + month.substr(-2) + '/' + year;
+        return time;
+    }
+
     getList(getRoute('persona_index'))
         .done(function(data) {
+            console.log("data", data);
             validateSession(data);
             $.each(data.resumen, function(i, item) {
                 $('#ganados-resumen-tipo-' + item.id).html(item.cantidad);
@@ -15,14 +28,12 @@
 
             $.each(data.response, function(i, item) {
                 tablaActual.row.add( [
-                    item.cedula,
+                    item.idRolFamilia.nombre,
                     '<a id="persona_' + item.id + '" name="lista_editar" href="javascript:loadModule(\'ganados\',\'ganados\',\'Editar\',' + item.id + ');">' + item.nombres + '</a>',
-                    item.apellidos,
-                    item.sexo,
-                    item.nacionalidad,
-                    item.iglesia.nombre,
-                    item.idEstatus.nombre,
-                    item.idEsCompleto.nombre,
+                    item.telefono,
+                    item.correo,
+                    item.metodoGanar.nombre,
+                    getFecha(item.fechaGanado.timestamp),
                 ] ).draw( false );
             });
         })
