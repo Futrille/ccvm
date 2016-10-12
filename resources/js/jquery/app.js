b9a0ee53888f501752a7a5df00713d655f3c6483
Daniel Futrillé <futridan@gmail.com>
2016-10-11 23:42:11 -0400

    Refactorización Tema SB Admin 2
    Se acomodo error de carga y simplificación de la carga del tema. Queda
    pendiente la carga del login.

--- resources/js/jquery/app.js
+++ resources/js/jquery/app.js
@@ -1,39 +1,41 @@
 (function() {
     'use strict';
 
-    //$.loader({ className:"blue-with-image-2", content:'' });
-    var jqxhr = $.post( getRoute('homepage') , $('form[name=login]').serialize(), function(data, status, xhr) {
-        console.log("data",data);
-        if (data.status != undefined && data.status == 'success'){
-            setMesageCode(MSG_NO_MESSAGE);
-            //window.location.href = getRoute();
-            cargarPanel();
-        }else if (data.status == 'logout'){
-            setMesageCode(MSG_LOGIN_ERROR);
-            printMessage(getMessageCode());
-            window.location.href = 'login.html';
-        }
-    })
-        .done(function() {
+    $.loader({ className:"blue-with-image-2", content:'' });
+    // var jqxhr = $.post( getRoute('homepage') , $('form[name=login]').serialize(), function(data, status, xhr) {
+    //     console.log("data",data);
+    //     if (data.status != undefined && data.status == 'success'){
+    //         setMesageCode(MSG_NO_MESSAGE);
+    //         //window.location.href = getRoute();
+             cargarPanel();
+    //     }else if (data.status == 'logout'){
+    //         setMesageCode(MSG_LOGIN_ERROR);
+    //         printMessage(getMessageCode());
+    //         window.location.href = 'login.html';
+    //     }
+    // })
+    // .done(function() {
+    //
+    // })
+    // .fail(function() {
+    //
+    // })
+    // .always(function() {
+    //     //$.loader('close');
+    // });
 
-        })
-        .fail(function() {
-
-        })
-        .always(function() {
-            //$.loader('close');
-        });
-    function cargarPanel(){
-        $('#wrapper').load('views/index/side-menu.html', hacerClick);
-    }
+     function cargarPanel(){
+         $('#ivm-side-menu').load('views/index/side-menu.html', hacerClick);
+         $('#ivm-header-menu').load('views/index/header-menu.html');
+         $('#ivm-top-right-menu').load('views/index/top-right-menu.html');
+     }
 
     function hacerClick(){
-        $("#btn-inicio").on('click', function(){
-            console.log('se hizo click');
-            $.loader('close');
-            loadModule('monitor','monitor','Index');
-        });
-        $( "#btn-inicio" ).trigger( "click" );
+         $("#btn-inicio").on('click', function(){
+             $.loader('close');
+             loadModule('monitor','monitor','Index');
+         });
+         $( "#btn-inicio" ).trigger( "click" );
     }
 })();
 
